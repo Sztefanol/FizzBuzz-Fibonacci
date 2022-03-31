a44619a86a18e98a0a4e2d64dfebfd05138c92e0
Sztefanol <luk266@gmail.com>
2022-03-31 22:33:45 +0200

    Make FizzBuzz read user input

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -5,11 +5,14 @@ public class Fibonacci {
     public static void main(String[] args) {
         ArrayList<Integer> fib = new ArrayList<>();
         Scanner s = new Scanner(System.in);
+
         fib.add(s.nextInt());
         fib.add(s.nextInt());
+        
         for(int i = 2; i < 12; i++) {
             fib.add(fib.get(i - 2) + fib.get(i - 1));
         }
+
         System.out.println(fib);
     }
 }
--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,11 +1,23 @@
+import javax.naming.PartialResultException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
         StringBuilder str = new StringBuilder();
+        ArrayList<Integer> arr = new ArrayList<>();
+
         for (int i = 1; i <= 10; i++) {
+            arr.add(s.nextInt());
+        }
+
+        for (int i: arr) {
             str.delete(0, str.length());
-            if(i % 3 == 0) str.append("Fizz");
-            if(i % 5 == 0) str.append("Buzz");
-            if(str.length() == 0) str.append(i);
+            if (i % 3 == 0) str.append("Fizz");
+            if (i % 5 == 0) str.append("Buzz");
+            if (str.length() == 0) str.append(i);
             System.out.println(str);
         }
     }
